359ca04436aa3e6156786c8d1c2682404ed35b72
developmentsoftwizinfotech <developmentsoftwizinfotech@users.noreply.github.com>
2024-02-08 09:26:54 +0000

    deploy: fbb94973abb7d78e260049b2329ea1005b3131fc

--- main.js
+++ main.js
@@ -4032,13 +4032,13 @@ var environment = {
   // basePDFUrl: 'http://localhost:59948/',
   baseApplicationUrl: 'https://api.veslink.com/',
   //Client URLs
-  //   baseApiUrl: 'https://cleanx7.murship.com/API/',
-  //  basePDFUrl: 'https://cleanx7.murship.com/API/',
+  baseApiUrl: 'https://cleanx7.murship.com/API/',
+  basePDFUrl: 'https://cleanx7.murship.com/API/'
   // baseApplicationUrl: 'https://api.veslink.com/',
   //Local URL
   //For FTP URL
-  baseApiUrl: 'http://dev-softwiz-002:9078/',
-  basePDFUrl: 'http://dev-softwiz-002:9078/'
+  // baseApiUrl: 'http://dev-softwiz-002:9078/',
+  // basePDFUrl: 'http://dev-softwiz-002:9078/',
   // baseApiUrl: 'http://mur-api.clientportal.net.in/',
   // basePDFUrl: 'http://mur-api.clientportal.net.in/',
 };
